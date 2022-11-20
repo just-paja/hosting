67591a5784912ce2748faa629a186ce9e45b620e
Pavel Žák <pavel@zak.global>
2022-11-21 01:36:07 +0200

    refactor: extract services module

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,31 +18,6 @@ provider "google" {
   credentials = var.GCP_CREDENTIALS
 }
 
-resource "google_project_service" "cloudresourcemanager" {
-  service = "cloudresourcemanager.googleapis.com"
-}
-
-resource "google_project_service" "iam" {
-  depends_on = [google_project_service.cloudresourcemanager]
-  service    = "iam.googleapis.com"
-}
-
-resource "google_project_service" "cloudsql" {
-  depends_on = [google_project_service.cloudresourcemanager]
-  service    = "sqladmin.googleapis.com"
-}
-
-resource "google_project_service" "cloudbuild" {
-  depends_on = [google_project_service.cloudresourcemanager]
-  service    = "cloudbuild.googleapis.com"
-}
-
-resource "google_project_service" "containerregistry" {
-  depends_on = [google_project_service.cloudresourcemanager]
-  service    = "containerregistry.googleapis.com"
-}
-
-resource "google_project_service" "kubernetes" {
-  depends_on = [google_project_service.cloudresourcemanager]
-  service    = "container.googleapis.com"
+module "services" {
+  source = "./modules/services"
 }
