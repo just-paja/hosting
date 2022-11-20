d5b605b8b1e86440fb9c1908ff15a5cac49fa04b
Pavel Žák <pavel@zak.global>
2022-11-21 01:23:25 +0200

    fix: cloud resource manager typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,31 +18,31 @@ provider "google" {
   credentials = var.GCP_CREDENTIALS
 }
 
-resource "google_project_service" "cloudresource" {
-  service = "cloudresource.googleapis.com"
+resource "google_project_service" "cloudresourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
 }
 
 resource "google_project_service" "iam" {
-  depends_on = [google_project_service.cloudresource]
+  depends_on = [google_project_service.cloudresourcemanager]
   service    = "iam.googleapis.com"
 }
 
 resource "google_project_service" "cloudsql" {
-  depends_on = [google_project_service.cloudresource]
+  depends_on = [google_project_service.cloudresourcemanager]
   service    = "sqladmin.googleapis.com"
 }
 
 resource "google_project_service" "cloudbuild" {
-  depends_on = [google_project_service.cloudresource]
+  depends_on = [google_project_service.cloudresourcemanager]
   service    = "cloudbuild.googleapis.com"
 }
 
 resource "google_project_service" "containerregistry" {
-  depends_on = [google_project_service.cloudresource]
+  depends_on = [google_project_service.cloudresourcemanager]
   service    = "containerregistry.googleapis.com"
 }
 
 resource "google_project_service" "kubernetes" {
-  depends_on = [google_project_service.cloudresource]
+  depends_on = [google_project_service.cloudresourcemanager]
   service    = "container.googleapis.com"
 }
