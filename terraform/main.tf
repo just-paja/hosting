50cd5e5fe57dcb05af6de3fd513743ca06793f30
Pavel Žák <pavel@zak.global>
2022-11-21 00:23:38 +0200

    feat: configure terraform base

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+variable "GCP_CREDENTIALS" { type = string }
+variable "GCP_PROJECT" { type = string }
+
+variable "GCP_REGION" {
+  type    = string
+  default = "europe-west1"
+}
+
+terraform {
+  backend "gcs" {
+    prefix = "hosting"
+  }
+}
+
+provider "google" {
+  project     = var.GCP_PROJECT
+  region      = var.GCP_REGION
+  credentials = var.GCP_CREDENTIALS
+}
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "cloudsql" {
+  service = "sqladmin.googleapis.com"
+}
+
+resource "google_project_service" "cloudbuild" {
+  service = "cloudbuild.googleapis.com"
+}
+
+resource "google_project_service" "containerregistry" {
+  service = "containerregistry.googleapis.com"
+}
+
+resource "google_project_service" "kubernetes" {
+  service = "container.googleapis.com"
+}
